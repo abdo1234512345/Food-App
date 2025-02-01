d9c736296048feced7efc84c6f0bfae8c14606a2
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-02-01 20:26:37 +0200

    first commit

---
+++ lib/constants/assets.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesBurger1
+  /// Assets/Images/burger1.png
+  static const String assetsImagesBurger1 = "Assets/Images/burger1.png";
+
+  /// Assets for assetsImagesBurger2
+  /// Assets/Images/burger2.png
+  static const String assetsImagesBurger2 = "Assets/Images/burger2.png";
+
+  /// Assets for assetsImagesBurger3
+  /// Assets/Images/burger3.png
+  static const String assetsImagesBurger3 = "Assets/Images/burger3.png";
+
+  /// Assets for assetsImagesBurger4
+  /// Assets/Images/burger4.png
+  static const String assetsImagesBurger4 = "Assets/Images/burger4.png";
+
+  /// Assets for assetsImagesIngeburger
+  /// Assets/Images/ingeburger.png
+  static const String assetsImagesIngeburger = "Assets/Images/ingeburger.png";
+
+  /// Assets for assetsImagesSplash screen
+  /// Assets/Images/Splash screen.png
+  static const String assetsImagesSplashscreen =
+      "Assets/Images/Splash screen.png";
+}
