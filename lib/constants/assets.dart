5af44402cd7bf34986d211013b89aa9e968d8dfa
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-02-03 19:17:20 +0200

    Adding App Images

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -18,12 +18,45 @@ class Assets {
   /// Assets/Images/burger4.png
   static const String assetsImagesBurger4 = "Assets/Images/burger4.png";
 
+  /// Assets for assetsImagesFoodgo
+  /// Assets/Images/Foodgo.png
+  static const String assetsImagesFoodgo = "Assets/Images/Foodgo.png";
+
+  /// Assets for assetsImagesFries
+  /// Assets/Images/fries.png
+  static const String assetsImagesFries = "Assets/Images/fries.png";
+
+  /// Assets for assetsImagesGroup 16
+  /// Assets/Images/Group 16.png
+  static const String assetsImagesGroup = "Assets/Images/Group 16.png";
+
   /// Assets for assetsImagesIngeburger
   /// Assets/Images/ingeburger.png
   static const String assetsImagesIngeburger = "Assets/Images/ingeburger.png";
+
+  /// Assets for assetsImagesOnion
+  /// Assets/Images/onion.png
+  static const String assetsImagesOnion = "Assets/Images/onion.png";
+
+  /// Assets for assetsImagesOnioncaramel 14
+  /// Assets/Images/onioncaramel 14.png
+  static const String assetsImagesOnioncaramel =
+      "Assets/Images/onioncaramel 14.png";
+
+  /// Assets for assetsImagesPickels
+  /// Assets/Images/pickels.png
+  static const String assetsImagesPickels = "Assets/Images/pickels.png";
+
+  /// Assets for assetsImagesSalad
+  /// Assets/Images/salad.png
+  static const String assetsImagesSalad = "Assets/Images/salad.png";
 
   /// Assets for assetsImagesSplash screen
   /// Assets/Images/Splash screen.png
   static const String assetsImagesSplashscreen =
       "Assets/Images/Splash screen.png";
+
+  /// Assets for assetsImagesTomato
+  /// Assets/Images/tomato.png
+  static const String assetsImagesTomato = "Assets/Images/tomato.png";
 }
