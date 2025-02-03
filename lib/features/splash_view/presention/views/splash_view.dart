4c2b7719054c6067613c325e847911053f547d7d
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-02-03 19:19:20 +0200

    Create Splash View To Delegate The Home View

---
+++ lib/features/splash_view/presention/views/splash_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:foodapp/constants/assets.dart';
+import 'package:foodapp/features/Home_view/presention/views/home_view.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomeView(),
+        ),
+      );
+    });
+    return Scaffold(
+      backgroundColor: Color(0xffEF2A39),
+      body: Image.asset(Assets.assetsImagesSplashscreen),
+    );
+  }
+}
