2aa0b59e5112aa54457c23196b7792c55281179a
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-02-03 19:40:02 +0200

    Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:foodapp/features/splash_view/presention/views/splash_view.dart';
 
 void main() => runApp(
       DevicePreview(
@@ -18,38 +19,7 @@ class FoodApp extends StatelessWidget {
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      home: SafeArea(child: SplashView()),
-    );
-  }
-}
-
-class SplashView extends StatelessWidget {
-  const SplashView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 4), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => HomeView(),
-        ),
-      );
-    });
-    return Scaffold(
-      backgroundColor: Color(0xffEF2A39),
-      body: Image.asset("Assets/Images/Splash screen.png"),
-    );
-  }
-}
-
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Placeholder(),
+      home: SplashView(),
     );
   }
 }
