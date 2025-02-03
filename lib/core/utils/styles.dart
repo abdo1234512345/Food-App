c045a753545b57c2bf75ac719d2278b62c776b33
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-02-03 19:16:44 +0200

    Adding App Styles

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const TextStyle robtomedian12 = TextStyle(
+    color: Color.fromARGB(0, 0, 0, 0),
+    fontSize: 12,
+    fontFamily: 'Roboto-Medium',
+  );
+
+  static const TextStyle robtomedian14 = TextStyle(
+    color: Color.fromARGB(0, 0, 0, 0),
+    fontSize: 14,
+    fontFamily: 'Roboto-Medium',
+  );
+  static const TextStyle robtomedian15 = TextStyle(
+    color: Color.fromARGB(0, 0, 0, 0),
+    fontSize: 15,
+    fontFamily: 'Roboto-Medium',
+  );
+  static const TextStyle robtomedian16 = TextStyle(
+    color: Color.fromARGB(0, 0, 0, 0),
+    fontSize: 16,
+    fontFamily: 'Roboto-Medium',
+  );
+  static const TextStyle robtomedian18 = TextStyle(
+    color: Color.fromARGB(0, 0, 0, 0),
+    fontSize: 18,
+    fontFamily: 'Roboto-Medium',
+  );
+  static const TextStyle robtoregular18 = TextStyle(
+    color: Color.fromARGB(0, 0, 0, 0),
+    fontSize: 18,
+    fontFamily: 'Roboto-Regular',
+  );
+  static const TextStyle lobsterregular45 = TextStyle(
+    color: Color.fromARGB(0, 0, 0, 0),
+    fontSize: 45,
+    fontFamily: 'Lobster-Regular',
+  );
+}
